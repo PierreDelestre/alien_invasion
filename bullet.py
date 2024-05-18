2801cf1ce7f80ebbe4e1fb9266bae524afa69116
Pierre Delestre <delestre.pierre@gmail.com>
2024-05-18 07:48:44 +0100

    Finish and completes game

--- bullet.py
+++ bullet.py
@@ -2,29 +2,30 @@ import pygame
 from pygame.sprite import Sprite
 
 class Bullet(Sprite):
-    """a class to manage bullets fired from the ship"""
+    """A class to manage bullets fired from the ship."""
 
     def __init__(self, ai_game):
-        """create a bullet object fired from the ship"""
+        """Create a bullet object at the ship's current position."""
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
 
-        # create a bullet rect at (0, 0) and the set correct position
-        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        # Create a bullet rect at (0, 0) and then set correct position.
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+            self.settings.bullet_height)
         self.rect.midtop = ai_game.ship.rect.midtop
 
-        # store bullet's position as a float
+        # Store the bullet's position as a float.
         self.y = float(self.rect.y)
 
     def update(self):
-        """move the bullet up the screen"""
-        # update exact position of the bullet
+        """Move the bullet up the screen."""
+        # Update the exact position of the bullet.
         self.y -= self.settings.bullet_speed
-        # update rect position
+        # Update the rect position.
         self.rect.y = self.y
 
     def draw_bullet(self):
-        """draw the bullet on the screen"""
+        """Draw the bullet to the screen."""
         pygame.draw.rect(self.screen, self.color, self.rect)
--- ship.py
+++ ship.py
@@ -1,39 +1,47 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship:
-    """A class to manage the ship"""
+
+class Ship(Sprite):
+    """A class to manage the ship."""
 
     def __init__(self, ai_game):
-        """Initialize the ship and set it starting position"""
+        """Initialize the ship and set its starting position."""
+        super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
-        # load the ship image and gets its rect
+        # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
-        # start each new ship at the bottom center of the screen
+        # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # store a float for the ship's exact horizontal position
+        # Store a float for the ship's exact horizontal position.
         self.x = float(self.rect.x)
 
-        # movement flag; start with a ship that's not moving
+        # Movement flags; start with a ship that's not moving.
         self.moving_right = False
         self.moving_left = False
-    
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
     def update(self):
-        """update the ship's position based on the movement flag"""
+        """Update the ship's position based on movement flags."""
+        # Update the ship's x value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
             
-        # update rect object from self.x
+        # Update rect object from self.x.
         self.rect.x = self.x
 
     def blitme(self):
-        """Draw the ship at its current position"""
+        """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
-        
